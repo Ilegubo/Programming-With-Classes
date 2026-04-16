b7eec03f6d135973072909e58a4611efe45cedee
Ilegubo <ilegubo@gmail.com>
2026-04-16 08:42:59 +0000

    W06 First commit

---
+++ week06/EternalQuest/SimpleGoal.cs
@@ -0,0 +1,31 @@
+using System;
+
+public class SimpleGoal : Goal
+{
+    private bool _isComplete;
+
+    public SimpleGoal(string name, string description, string points) : base(name, description, points)
+    {
+        _isComplete = false;
+    }
+
+    public SimpleGoal(string name, string description, string points, bool isComplete) : base(name, description, points)
+    {
+        _isComplete = isComplete;
+    }
+
+    public override void RecordEvent()
+    {
+        _isComplete = true;
+    }
+
+    public override bool IsComplete()
+    {
+        return _isComplete;
+    }
+
+    public override string GetStringRepresentation()
+    {
+        return $"SimpleGoal:{_shortName},{_description},{_points},{_isComplete}";
+    }
+}
